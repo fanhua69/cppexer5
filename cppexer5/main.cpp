cb52340ecfeb4b19f422c3ceb3b1fe3ef70e056c
David Fan <fanhua69@gmail.com>
2025-11-27 10:57:21 -0700

    Initial Commit

---
+++ cppexer5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cppexer5
+//
+//  Created by hua fan on 2025-11-27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
